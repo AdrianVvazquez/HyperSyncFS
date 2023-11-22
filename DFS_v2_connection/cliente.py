fba3eeef317db1f68c140d408a06f7331863a46b
AdrianVvazquez <jose_a_vazquez@outlook.com>
2023-11-21 22:36:22 -0600

    Servidor RPC y Cliente con librería rpyc

--- DFS_v2_connection/cliente.py
+++ DFS_v2_connection/cliente.py
@@ -1,3 +1,12 @@
 import rpyc
-conn = rpyc.classic.connect("localhost")
 
+c = rpyc.connect("localhost", 18861)
+
+# with open("new_doc2.txt", "x") as f:
+#     f.write("Halo\n")
+
+# the service is exposed as the root object of the connection
+# print(c.root)
+# print(c.root.create_file(f))
+# print(c.root.get_files())
+print(c.root.listdir("/Users"))
