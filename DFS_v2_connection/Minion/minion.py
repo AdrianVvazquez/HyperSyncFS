d0cf5e7066f6adcf147c37eabc822b2d48182873
AdrianVvazquez <jose_a_vazquez@outlook.com>
2023-11-25 20:37:00 -0600

    Conexión a Minion-Master

--- DFS_v2_connection/Minion/minion.py
+++ DFS_v2_connection/Minion/minion.py
@@ -3,21 +3,13 @@ import rpyc
 import uuid
 import math
 
-# Generate a random UUID
-CHUNKS_SIZE = 100
 class Minion(rpyc.Service):
-    Connections = []
     My_files = []   # Lista con nombres de los archivos
-                    # se puede acceder a él mediante el nombre de la clase o el nombre de una instancia de la clase.
-    # my_new_files = self.show_files()
-
-    def __init__(self):
-        self.Chunks = {}
-        self.Partitions = []
-
+    Chunks = {}
+    Partitions = []
+    
     def on_connect(self, conn):
         print(f"[Conexión nueva] {conn}")
-        self.Connections.append(conn)
         # code that runs when a connection is created
     
     def on_disconnect(self, conn):
@@ -27,13 +19,19 @@ class Minion(rpyc.Service):
     def exposed_get_files(self): # this is an exposed method
         return self.My_files
 
-    def save_chunks(self, chunks): # this is an exposed method
+    def Connect_to_Master(self, chunks): # this is an exposed method
         print(chunks)
         # return self.My_files
 
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
+    # Conectarse a Master
+    remoteMaster = rpyc.connect('localhost', 18861)
+    serviceMaster = remoteMaster.root
+    minion_id = serviceMaster.Add_Minion()
+    print(minion_id)
+
     t = ThreadedServer(Minion, port=18862, protocol_config={
         'allow_public_attrs': True,
     })
